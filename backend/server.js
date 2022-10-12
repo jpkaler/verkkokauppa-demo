0b9748b516f609ec40880f4386a2ab634599e1f9
Juho Kalermo <juho.kalermo@gmail.com>
2022-10-12 14:12:21 +0300

    GET one item

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,32 @@ let app = express();                    //
 app.use(express.json());                //kaikki menee expressin kautta
 
 //dummy database
-let database = [];
-let id = 1;
+let database = [{
+    id:1,
+    nimi:"farkut",
+    hinta:19.99,
+    kategoria:"housut"
+},{
+    id:2,
+    nimi:"t-paita",
+    hinta:29.99,
+    kategoria:"paidat"
+}];
 
+// GET -> koko tuotetietokanta
 app.get("/api/verkkokauppa", (req,res) => {         // function(request,response) on sama kuin (req,res) =>
     return res.status(200).json(database);          
 });
+
+// GET -> yksi tuote tietokannasta
+app.get("/api/verkkokauppa/:productId", (req, res) => {
+    const foundItem = database.find((product) => product.id === Number(req.params.productId)); // Tallentaa tuotteen muuttujaan 'foundItem', jos id löytyy tietokannasta
+    if (foundItem) {
+        return res.status(200).json(foundItem); // Palauttaa tuotteen
+    } else {
+        return res.status(404).json({Message:"Item not found"});
+    }
+})
 
 //dev portti
 let port = process.env.PORT || 3001;
