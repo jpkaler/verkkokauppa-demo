4679cfa1e957c795a30e4a6e1bdf4d3e3c4250c3
Juho Kalermo <juho.kalermo@gmail.com>
2022-10-13 11:11:17 +0300

    react boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  
+
+
+
+  return (
+    <h1>Verkkokauppa</h1>
+  );
+}
+
+export default App;
