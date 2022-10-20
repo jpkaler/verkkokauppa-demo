2feb4a3cff8d2469e46852a49bc383fbeea46af6
Juho Kalermo <juho.kalermo@gmail.com>
2022-10-20 11:32:53 +0300

    CRUD methods

--- backend/server.js
+++ backend/server.js
@@ -5,61 +5,32 @@ const sqlite3 = require('sqlite3');
 let app = express();                    //
 app.use(express.json());                //kaikki menee expressin kautta
 
- 
-/* // Dummy Database
-let tempDatabase = [{
-    id:1,
-    nimi:"farkut",
-    hinta:19.99,
-    kategoria:"housut"
-},{
-    id:2,
-    nimi:"t-paita",
-    hinta:29.99,
-    kategoria:"paidat"
-}]; */
-
 // Product Database
 
 const db = new sqlite3.Database('../products.db');
 
 
-// GET -> koko tuotetietokanta
-app.get("/api/verkkokauppa", (req,res) => {  // function(request,response) on sama kuin (req,res) =>
-    
-    db.all('SELECT * FROM products', (err, rows) => { 
-        return res.status(200).json(rows);
-    })
-
-
-});
-/* 
-
-//GET -> kaikki kategoriat
-app.get("/api/verkkokauppa/kategoriat", (req,res) => {
-    db.all('SELECT category FROM products WHERE category NOT NULL GROUP BY category', (err, rows) => {
-
-        return res.status(200).send(rows);
-    })
-})
-
-//GET -> yhden kategorian tuotteet
-app.get("/api/verkkokauppa/kategoriat/:category", (req,res) => {
-
-    db.all('SELECT * FROM products WHERE category=$category', 
-
-        {
-            $category: req.params.category
-        }, (err, rows) => {
-            if (rows.length === 0) {
-                return res.status(404).json({message: "Category not found"});
-            } 
+// GET -> hakukriteerin mukainen tuotetietokanta
+app.get("/api/verkkokauppa/", (req,res) => {  
+    if (req.query.search || req.query.category) {
+        db.all("SELECT * FROM products WHERE name LIKE $search", {
+            $search: '%' + req.query.search + '%'
+        },
+             (err, rows) => {
+                if (!rows[0]) {
+                    return res.status(404).json({message:"No products found"})
+                }
+                return res.status(200).json(rows);
+            }
+        )
+    } else {
+        db.all("SELECT * FROM products", (err, rows) => {
             return res.status(200).json(rows);
         })
+    }
+});
 
-})
-
-// GET -> yksi tuote tietokannasta SQLite
+// GET -> yksi tuote tietokannasta
 app.get("/api/verkkokauppa/:productId", (req, res) => {
     
 
@@ -75,7 +46,50 @@ app.get("/api/verkkokauppa/:productId", (req, res) => {
             
         })
 })
- */
+
+// POST -> luo uusi tuote tietokantaan
+app.post("/api/verkkokauppa", (req,res) => {
+    db.run('INSERT INTO products (name, price, category) VALUES ($name, $price, $category)', {
+        $name: req.body.name,
+        $price: req.body.price,
+        $category: req.body.category
+    }, (err) => {
+        if (err) {
+            console.log(err.message);
+            res.status(409).json({message: "Invalid product values"});
+        } else {
+            res.status(201).json({message: "New product added to the database"});
+        }
+    })
+})
+
+// DELETE -> Poistaa halutun tuotteen tietokannasta
+app.delete("/api/verkkokauppa/:productId", (req,res) => {
+    db.run('DELETE FROM products WHERE ID = $productId', {
+        $productId: req.params.productId
+    }, (err) => {
+        res.status(200).json({message: "Product deleted successfully"});
+        }
+    )
+})
+
+// PUT -> Muokkaa valitun tuotteen tietoja tietokannassa
+app.put("/api/verkkokauppa/:productId", (req,res) => {
+    db.run('UPDATE products SET name=$name, price=$price, category=$category WHERE ID = $productId', {
+        $name: req.body.name,
+        $price: req.body.price,
+        $category: req.body.category,
+        $productId: req.params.productId
+    }, (err) => {
+        if (err) {
+            console.log(err.message);
+            res.status(404).json({message: "Invalid update values"});
+        } else {
+            res.status(200).json({message: "Product updated successfully"});
+        }
+    })
+})
+
 //dev portti
 let port = process.env.PORT || 3001;
 app.listen(port);
