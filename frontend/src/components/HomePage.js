1685222ade4d4460d6dfd0850ea09b67d6593292
risa562 <63837480+risa562@users.noreply.github.com>
2022-11-25 10:59:41 +0200

    UI 1.0.2

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -15,11 +15,11 @@ const HomePage = (props) => {
                 </CCol>
                 <CCol md="8">
                 </CCol>
-                <CContainer fluid>
+                <CContainer fluid className="category-bar">
                     <h2 style={{margin: "20px", color:"white"}}>Tervetuloa verkkokauppa-demoon!</h2>
-                    <CRow><p style={{margin: "20px", color:"white", maxWidth:"35%", textAlign:"justify", fontSize:"14px"}}>Tällä sivustolla voit elää shoppailijan unelmaa ja selailla lukuisia tuotteita useista eri kategorioista. Tuotteet löytyvät sekä kategoriasivuilta että hakukentän avulla. Kokeile lisätä tuotteita ostoskoriin ja muokata ostoskoria itsellesi mieluisammaksi. Voit myös luoda itsellesi käyttäjätunnuksen rekisteröitymissivulta ja kirjautua sisään sivustolle.</p>
+                    <CRow><p style={{margin: "16px", color:"white", maxWidth:"60%", textAlign:"justify", fontSize:"14px"}}>Tällä sivustolla voit elää shoppailijan unelmaa ja selailla lukuisia tuotteita useista eri kategorioista. Tuotteet löytyvät sekä kategoriasivuilta että hakukentän avulla. Kokeile lisätä tuotteita ostoskoriin ja muokata ostoskoria itsellesi mieluisammaksi. Voit myös luoda itsellesi käyttäjätunnuksen rekisteröitymissivulta ja kirjautua sisään sivustolle.</p>
                     </CRow>
-                    <CRow><p style={{margin: "20px", color:"white", maxWidth:"35%", textAlign:"justify", fontSize:"14px"}}>Tämä sovellus on Opiframen järjestämän OpiKoodia -koulutuksen loppuprojekti. Tekijöinä Juho Kalermo ja Riku Sänkiaho.</p>
+                    <CRow><p style={{margin: "16px", color:"white", maxWidth:"60%", textAlign:"justify", fontSize:"14px"}}>Tämä sovellus on Opiframen järjestämän OpiKoodia -koulutuksen loppuprojekti. Tekijöinä Juho Kalermo ja Riku Sänkiaho.</p>
                     </CRow>
                 </CContainer>   
         </CContainer>   
