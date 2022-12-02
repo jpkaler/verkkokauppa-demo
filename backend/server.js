e052d030803be73da3751a15bb3fb6efb894770b
Juho Kalermo <juho.kalermo@gmail.com>
2022-12-02 10:34:42 +0200

    sessiondata

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(
         cookie:{ maxAge: 1000 * 60 * 60},
         resave: false,
         saveUnitialized: false,
-        store: new SQLiteStore({ db: 'sessions.db', dir: './db' })
+        store: new SQLiteStore({ db: 'sessions.db', dir: '../' })
     })
 )
 
